f5159b569d0a4f66f9f371363a486192cb3aeffa
FranLinares06 <167379538+FranLinares06@users.noreply.github.com>
2024-04-20 09:56:11 -0600

    Update cintametrica.h
    Agregando codigo de archivo cintametrica.h

---
+++ cintametrica/cintametrica.h
@@ -0,0 +1,36 @@
+#ifndef cintametrica_h
+#define cintametrica_h
+#include "arduino.h"
+class Ultrasonido{
+    private:
+        long distancia;
+    public:
+        byte pinEcho;
+        byte pinTrigger;
+
+    Ultrasonido (byte _pinTrigger, byte _pinEcho);
+    Ultrasonido();
+    int medirCM();
+};
+class Display7{
+    private:
+        byte matrix[10][7]={
+        {1,1,1,1,1,1,0}, //cero
+        {0,1,1,0,0,0,0}, //uno
+        {1,1,0,1,1,0,1}, //dos
+        {1,1,1,1,0,0,1}, //tres
+        {0,1,1,0,0,1,1}, //cuatro
+        {1,0,1,1,0,1,1}, //cinco
+        {1,0,1,1,1,1,1}, //seis
+        {1,1,1,0,0,0,0}, //siete
+        {1,1,1,1,1,1,1}, //ocho
+        {1,1,1,0,0,1,1} //nueve
+    };
+    public:
+        byte pines[7];
+        //Se configura utilizando un metodo
+        void configurar(byte[]);
+        //dibuja el numero en el display
+        void mostrar(byte num);
+};
+#endif
